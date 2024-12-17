71d235ceb415e48f6a0d5d7af17e0483ecd9d7ac
Анна Земскова <anuta.nyta1012@yandex.ru>
2024-12-18 00:49:56 +0500

    fix5

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,60 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
+import path from 'path';
+import autoprefixer from 'autoprefixer';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const config = {
+export const module = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    path: path.resolve(dirname, 'dist'),
   },
   devServer: {
+    static: path.resolve(dirname, 'dist'),
+    port: 8080,
+    hot: true,
     open: true,
     host: 'localhost',
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
+    new HtmlWebpackPlugin({ template: './index.html' }),
   ],
   module: {
     rules: [
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader',
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader',
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  autoprefixer,
+                ],
+              },
+            },
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader',
+          },
+        ],
       },
     ],
   },
 };
 
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-  } else {
-    config.mode = 'development';
-  }
-  return config;
-};
-
-module.exports = {
-  entry: './src/js/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    hot: true,
-  },
-};
+export default module;
