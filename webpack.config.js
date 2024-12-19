25d8dcba5b17f9a16d89996b95eba702197bce56
Анна Земскова <anuta.nyta1012@yandex.ru>
2024-12-20 01:09:10 +0500

    environment setup2

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,37 @@
+import path from 'path';
+// import autoprefixer from 'autoprefixer';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
 
-export default {
-  mode: process.env.NODE_ENV || 'development',
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export const module = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: path.resolve(dirname, 'dist'),
+    port: 8080,
+    hot: true,
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+  ],
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-  ],
-  output: {
-    clean: true,
-  },
 };
+
+export default module;
