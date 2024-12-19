866ad180ede58b91767e688a5a0d7004d83e248b
Анна Земскова <anuta.nyta1012@yandex.ru>
2024-12-19 15:40:12 +0500

    fix15

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+import path from 'path';
+import autoprefixer from 'autoprefixer';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export const module = {
+  mode: 'development',
+  entry: 'src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(dirname, 'dist'),
+  },
+  devServer: {
+    static: path.resolve(dirname, 'dist'),
+    port: 8080,
+    hot: true,
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  autoprefixer,
+                ],
+              },
+            },
+          },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+};
+
+export default module;
