f86bc7e42894ac9b196fea00889bb08e6d6b0753
Анна Земскова <anuta.nyta1012@yandex.ru>
2024-12-24 14:52:23 +0500

    fix13

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,3 @@
 export default {
-  // Add you postcss configuration here
-  // Learn more about it at https://github.com/webpack-contrib/postcss-loader#config-files
   plugins: [['autoprefixer']],
 };
